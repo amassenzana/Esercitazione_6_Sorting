3f7dc072a5c81758ffe6c7ef4e8661ce028bcc2e
AlessioMassenzana <s273845@studenti.polito.it>
2024-05-04 14:01:22 +0200

    Completato Esercizio 2

--- Esercizio_2/main.cpp
+++ Esercizio_2/main.cpp
@@ -1,74 +1,155 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
-#include <unistd.h>
-#include <numeric>
 #include <algorithm>
 #include "SortingAlgorithm.hpp"
+#include <iomanip>
 
 using namespace std;
 
-// void printvec(vector<int>& v){
-//     int n = v.size();
-//     for(int i = 0; i < n; i++){
-//         cout << v[i] << " ";
-//     }
-//     cout << endl;
-// }
-
-void testSort(vector<int>& vec){
+void testSort(vector<int>& vec, double& tB, double& tM){
     vector<int> v = vec;
 
     std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
     SortLibrary::BubbleSort(v);
     std::chrono::steady_clock::time_point t_end = std::chrono::steady_clock::now();
-    double duration = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
-    cout << "\tBS" << ": " << duration << "ms";
+    tB = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
+
 
     v = vec;
 
     t_begin = std::chrono::steady_clock::now();
     SortLibrary::MergeSort(v);
     t_end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
-    cout << "\tMS" << ": " << duration << "ms";
+    tM = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
+
 }
 
 
 
 int main(int argc, char **argv){
+    if (argc < 2){
+        cerr << "Inserisci la dimensione da linea di comando";
+        return 0;
+    }
 
     int n = stoi(argv[1]);
     cout << "Dimension of the problem: " << n << endl;
     vector<int> v1(n);
     int k;
     srand(time(NULL));
+    double tB, tM;
 
-    cout << "\nReverse vector:\t\t";
-    k = n;
-    generate(v1.begin(), v1.end(), [&k](){return k--;});
-    testSort(v1);
-
-    cout << "\nOrdinate vector:\t\t";
+    cout << "-----First attempt-----" << endl;
+    cout << "Ordinate vector:\t\t";
     k = 0;
     generate(v1.begin(), v1.end(), [&k](){return k++;});
-    testSort(v1);
+    testSort(v1, tB, tM);
+    cout << "\tBS" << ": " << tB << "ms";
+    cout << "\tMS" << ": " << tM << "ms" << endl;
 
 
-    cout << "\nRandom Vector (0,n):\t";
+    cout << "Reverse vector:\t\t";
+    k = n;
+    generate(v1.begin(), v1.end(), [&k](){return k--;});
+    testSort(v1, tB, tM);
+    cout << "\tBS" << ": " << tB << "ms";
+    cout << "\tMS" << ": " << tM << "ms" << endl;
+
+
+    cout << "Random Vector (0,n):\t";
     generate(v1.begin(), v1.end(), [&n](){return rand()%n;});
-    testSort(v1);
+    testSort(v1, tB, tM);
+    cout << "\tBS" << ": " << tB << "ms";
+    cout << "\tMS" << ": " << tM << "ms" << endl;
 
-    cout << "\nRandom Vector (0,500):\t";
+
+    cout << "Random Vector (0,500):\t";
     generate(v1.begin(), v1.end(), [&n](){return rand()%501;});
-    testSort(v1);
+    testSort(v1, tB, tM);
+    cout << "\tBS" << ": " << tB << "ms";
+    cout << "\tMS" << ": " << tM << "ms" << endl;
 
-    cout << "\nRandom Vector (0,2000):";
+
+    cout << "Random Vector (0,2000):";
     generate(v1.begin(), v1.end(), [&n](){return rand()%2000;});
-    testSort(v1);
+    testSort(v1, tB, tM);
+    cout << "\tBS" << ": " << tB << "ms";
+    cout << "\tMS" << ": " << tM << "ms" << endl;
 
 
-    cout << "\n";
+
+
+    cout << "\n\n----Calculating mean over 30 test----" << endl;
+    cout << fixed << setprecision(1);
+    double totalBS = 0, totalMS = 0; k = n;
+
+    // ORDINATED VECTOR
+    totalBS = 0; totalMS = 0; k = 0;
+    generate(v1.begin(), v1.end(), [&k](){return k++;});
+    for(size_t i = 0; i < 30; i++){
+        testSort(v1, tB, tM);
+        totalBS += tB;
+        totalMS += tM;
+    }
+    cout << "Ordinated vector:\t\t"
+         << "\tBS" << ": " << totalBS/30 << "ms"
+         << "\t\tMS" << ": " << totalMS/30 << "ms" << endl;
+
+
+    // REVERSE VECTOR
+    generate(v1.begin(), v1.end(), [&k](){return k--;});
+    for(size_t i = 0; i < 30; i++){
+        testSort(v1, tB, tM);
+        totalBS += tB;
+        totalMS += tM;
+    }
+    cout << "Reverse vector:\t\t"
+         << "\tBS" << ": " << totalBS/30 << "ms"
+         << "  \tMS" << ": " << totalMS/30 << "ms" << endl;
+
+
+
+    // RANDOM VECTOR [0,n]
+    totalBS = 0; totalMS = 0;
+    for(size_t i = 0; i < 30; i++){
+        generate(v1.begin(), v1.end(), [&n](){return rand()%n;});
+        testSort(v1, tB, tM);
+        totalBS += tB;
+        totalMS += tM;
+    }
+    cout << "Random Vector (0,n):\t"
+         << "\tBS" << ": " << totalBS/30 << "ms"
+         << "  \tMS" << ": " << totalMS/30 << "ms" << endl;
+
+
+    // RANDOM VECTOR [0,500]
+    totalBS = 0; totalMS = 0;
+    for(size_t i = 0; i < 30; i++){
+        generate(v1.begin(), v1.end(), [&n](){return rand()%501;});
+        testSort(v1, tB, tM);
+        totalBS += tB;
+        totalMS += tM;
+    }
+    cout << "Random Vector (0,500):\t"
+         << "\tBS" << ": " << totalBS/30 << "ms"
+         << "  \tMS" << ": " << totalMS/30 << "ms" << endl;
+
+
+    // RANDOM VECTOR [0,2000]
+    totalBS = 0; totalMS = 0;
+    for(size_t i = 0; i < 30; i++){
+        generate(v1.begin(), v1.end(), [&n](){return rand()%2001;});
+        testSort(v1, tB, tM);
+        totalBS += tB;
+        totalMS += tM;
+    }
+    cout << "Random Vector (0,2000):"
+         << "\tBS" << ": " << totalBS/30 << "ms"
+         << "  \tMS" << ": " << totalMS/30 << "ms" << endl;
+
+
+
     return 0;
 }
 
